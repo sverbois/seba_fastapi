a9711abbefa7ff1a9e9736376e19ce67645a2c8b
Sébastien Verbois <sebastien.verbois@imio.be>
2021-03-05 06:16:51 +0100

    Add 'add-to-42' app

---
+++ app2/main.py
@@ -0,0 +1,25 @@
+import json
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+api = FastAPI()
+
+
+@api.get("/add-to-42")
+def add_to_42_query(number: int):
+    return 42 + number
+
+
+@api.get("/add-to-42/{number}")
+def add_to_42_path(number: int):
+    return 42 + number
+
+
+class BodyModel(BaseModel):
+    number: int
+
+
+@api.post("/add-to-42")
+def add_to_42_body(body: BodyModel):
+    return 42 + body.number
