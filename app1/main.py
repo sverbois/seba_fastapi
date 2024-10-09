3eedc83397ffc7fdbbb0b00956da53718eda2dd6
Sébastien Verbois <sebastien.verbois@imio.be>
2024-10-09 12:56:36 +0200

    Update app1

--- app1/main.py
+++ app1/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Annotated
 
 from fastapi import Body
 from fastapi import FastAPI
@@ -15,16 +15,17 @@ def root():
 
 
 @api.get("/hello")
-def hello(name: str = Query(...)):
-    # def hello(name: str):
-    # def hello(name):
+def hello(
+    name: Annotated[str, Query()],  # ou name: str
+):
     message = f"Hello {name} !!!"
     return {"message": message}
 
 
 @api.get("/hello-optional")
-def hello_optional(name: str = Query(None)):
-    # def hello(name: Optional(str) = None):
+def hello_optional(
+    name: Annotated[str | None, Query()] = None,
+):
     if name is None:
         name = "stranger"
     message = f"Hello {name} !!!"
@@ -32,28 +33,31 @@ def hello_optional(name: str = Query(None)):
 
 
 @api.get("/items/{item_id}")
-def read_item(item_id: str = Path(...)):
-    # def read_item(item_id: str):
-    # def read_item(item_id):
+def read_item(
+    item_id: Annotated[int, Path()],  # ou item_id: int
+):
     return {"item_id": item_id}
 
 
 @api.get("/headers")
-def get_user_agent(user_agent: str = Header(...)):
+def get_user_agent(
+    user_agent: Annotated[str, Header()],
+):
     return {"User-Agent": user_agent}
 
 
 @api.post("/items")
-def create_item(item: dict = Body(...)):
-    # def create_item(item: dict):
+def create_item(
+    item: Annotated[dict, Body()],  # ou item: dict
+):
     return {"item": item}
 
 
 @api.post("/all/{item_id}")
 def return_all_parameters(
-    item_id: str = Path(...),
-    name: str = Query(...),
-    user_agent: str = Header(...),
-    item: dict = Body(...),
+    item_id: Annotated[int, Path()],
+    user_agent: Annotated[str, Header()],
+    item: Annotated[dict, Body()],
+    name: Annotated[str, Query()],
 ):
     return {"name": name, "item_id": item_id, "user_agent": user_agent, "item": item}
