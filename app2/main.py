268470fd304e82ac865e31b56fb8996a20a8f952
Sébastien Verbois <sebastien.verbois@imio.be>
2024-10-09 13:21:48 +0200

    Update app2

--- app2/main.py
+++ app2/main.py
@@ -1,16 +1,25 @@
+from typing import Annotated
+
+from fastapi import Body
 from fastapi import FastAPI
+from fastapi import Path
+from fastapi import Query
 from pydantic import BaseModel
 
 api = FastAPI()
 
 
 @api.get("/add-to-42")
-def add_to_42_query(number: int):
+def add_to_42_query(
+    number: Annotated[int, Query()],
+):
     return 42 + number
 
 
 @api.get("/add-to-42/{number}")
-def add_to_42_path(number: int):
+def add_to_42_path(
+    number: Annotated[int, Path()],
+):
     return 42 + number
 
 
@@ -20,5 +29,7 @@ class BodyModel(BaseModel):
 
 
 @api.post("/add-to-42")
-def add_to_42_body(body: BodyModel):
+def add_to_42_body(
+    body: Annotated[BodyModel, Body()],
+):
     return 42 + body.number1 + body.number2
